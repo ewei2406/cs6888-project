90110bc09fac5492701b4f4b0e59fffe330f051b
Edward Wei <nyw6dh@virginia.edu>
2024-04-10 00:03:37 -0400

    t

--- databoss.py
+++ databoss.py
@@ -1,4 +1,51 @@
+import atexit
 import subprocess
+from dataclasses import dataclass
+import os
+
+@dataclass
+class CommandOutput:
+  status: int=-1
+  out: str=""
+  err: str="Not implemented!"
+  
+  @classmethod
+  def from_subprocess(cls, result: subprocess.CompletedProcess[str]):
+    return cls(result.returncode, result.stdout, result.stderr)
+  
+  def __repr__(self) -> str:
+    return f"STATUS: {self.status}\tOUT: {self.out}\tERR: {self.err}\n"
+
+
+class Driver:
+  def __init__(self) -> None:
+    return
+  def exit(self) -> None:
+    return None;
+  def run(self, sql: str):
+    return None
+
+
+class SQLiteDriver(Driver):
+  def run(self, sql: str) -> CommandOutput:
+    result = subprocess.run(['sqlite3/sqlite3', 'tmp/db.sqlite', sql], text=True, capture_output=True)
+    return CommandOutput.from_subprocess(result)
+
+
+class PostgresDriver(Driver):
+  def __init__(self) -> None:
+    super().__init__()
+    # Create the postgres stuff
+    if len(os.listdir('tmp/postgres')) == 0:
+      subprocess.run(['initdb', 'tmp/postgres'])
+    subprocess.run(["pg_ctl", "-D", "tmp/postgres", "start"], check=True, capture_output=False)
+    
+  def exit(self) -> None:
+    subprocess.run(["pg_ctl", "-D", "tmp/postgres", "stop"], check=True)
+
+  def run(self, sql: str) -> CommandOutput:
+    result = subprocess.run(['psql', '-d', 'my_db', '-c', sql], check=True, capture_output=True)
+    return CommandOutput.from_subprocess(result)
 
 
 q1 = """
@@ -15,14 +62,18 @@ SELECT * FROM my_table2
 """
 
 def main():
-  args = ["./sqlite3/sqlite3", "db.sqlite", q1]
-  return_code = subprocess.call(args)
-  print(return_code)
-  args = ["./sqlite3/sqlite3", "db.sqlite", q2]
-  subprocess.call(args)
-  args = ["./sqlite3/sqlite3", "db.sqlite", q3]
-  return_code = subprocess.call(args)
-  print(return_code)
+  pg = PostgresDriver()
+  r1 = pg.run(q1)
+  r2 = pg.run(q2)
+  r3 = pg.run(q3)
+  print(r1, r2, r3)
+  
+  # r1 = SQLiteCommand(q1).run()
+  # r2 = SQLiteCommand(q2).run()
+  # r3 = SQLiteCommand(q3).run()
+  # print(r1, r2, r3)
+  
+  pg.exit()
 
 if __name__ == "__main__":
   main()
